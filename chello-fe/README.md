6c39e36e8ffa1323b7bd66c67a1e296c727ead98
Tien Thanh <nguyentienthanh.tgdd@gmail.com>
2022-12-20 15:56:53 +0700

    Header unfinished

--- chello-fe/README.md
+++ chello-fe/README.md
@@ -1,4 +1,3 @@
-
 ## Available Scripts
 
 In the project directory, you can run:
@@ -37,4 +36,5 @@ Instead, it will copy all the configuration files and the transitive dependencie
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
 ## WBS
-![alt text](https://i.ibb.co/xMdsc1K/WBS.png)
+
+![alt text](https://i.ibb.co/3mWs016/wbs.png)
---
+++ chello-fe/src/components/Header.tsx
@@ -0,0 +1,282 @@
+import React from "react";
+import { useState } from "react";
+import {
+  FaBell,
+  FaSearch,
+  FaCaretDown,
+  FaTasks,
+  FaPlus,
+  FaTable,
+  FaUsers,
+  FaLayerGroup,
+  FaPaintRoller,
+} from "react-icons/fa";
+import "../App.css";
+
+const workspace = {
+  myWorkspace: [
+    {
+      name: "Không gian 1",
+      img: "https://thuthuatnhanh.com/wp-content/uploads/2021/11/hinh-anh-chill-dep.jpg",
+    },
+    {
+      name: "Lịch học",
+      img: "https://thuthuatnhanh.com/wp-content/uploads/2021/11/hinh-anh-chill-dep.jpg",
+    },
+    {
+      name: "Project",
+      img: "https://thuthuatnhanh.com/wp-content/uploads/2021/11/hinh-anh-chill-dep.jpg",
+    },
+  ],
+  favWorkspace: [
+    {
+      name: "Không gian 2",
+      img: "https://thuthuatnhanh.com/wp-content/uploads/2021/11/hinh-anh-chill-dep.jpg",
+    },
+    {
+      name: "Đồ án",
+      img: "https://thuthuatnhanh.com/wp-content/uploads/2021/11/hinh-anh-chill-dep.jpg",
+    },
+    {
+      name: "Assignment",
+      img: "https://thuthuatnhanh.com/wp-content/uploads/2021/11/hinh-anh-chill-dep.jpg",
+    },
+  ],
+};
+
+const Header = () => {
+  const [theme, setTheme] = useState("default");
+
+  const changeTheme = (theme: string) => {
+    setTheme(theme);
+  };
+
+  return (
+    <header className={theme}>
+      <div className="navbar">
+        {/* Left */}
+        <div className="navbar-left">
+          <a href="/">
+            <FaTasks className="logo-icon" />
+            <span>Chello</span>
+          </a>
+          <div className="navbar-responsive">
+            <div className="navbar-responsive-toggle">
+              <span>Thanh công cụ</span>
+              <span>
+                <FaCaretDown />
+              </span>
+            </div>
+            <ul className="navbar-responsive-item">
+              <li>
+                <span>Các không gian làm việc</span>
+                <span>
+                  <FaCaretDown />
+                </span>
+                <div className="navbar-dropdown">
+                  <ul>
+                    <h5>Không gian của tôi</h5>
+                    {workspace.myWorkspace.map((ws, index) => (
+                      <li key={index}>
+                        <img src={ws.img} alt="img" />
+                        <span>{ws.name}</span>
+                      </li>
+                    ))}
+                    <h5>Không gian ưa thích</h5>
+                    {workspace.favWorkspace.map((ws, index) => (
+                      <li key={index}>
+                        <img src={ws.img} alt="img" />
+                        <span>{ws.name}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <span>Gần đây</span>
+                <span>
+                  <FaCaretDown />
+                </span>
+                <div className="navbar-dropdown">
+                  <ul>
+                    <h5>Đã xem gần đây</h5>
+                    {workspace.myWorkspace.map((ws, index) => (
+                      <li key={index}>
+                        <img src={ws.img} alt="img" />
+                        <span>{ws.name}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <span>Ưa thích</span>
+                <span>
+                  <FaCaretDown />
+                </span>
+                <div className="navbar-dropdown">
+                  <ul>
+                    {workspace.myWorkspace.map((ws, index) => (
+                      <li key={index} className="navbar-dropdown-star-items">
+                        <div>
+                          <img src={ws.img} alt="img" />
+                          <span>{ws.name}</span>
+                        </div>
+                        <div
+                          onClick={() => alert("Đã hủy")}
+                          title="Nhấn để xóa khỏi danh sách ưa thích"
+                        ></div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <span>Mẫu</span>
+                <span>
+                  <FaCaretDown />
+                </span>
+                <div className="navbar-dropdown">
+                  <ul>
+                    <h5>Các mẫu hàng đầu</h5>
+                    {workspace.myWorkspace.map((ws, index) => (
+                      <li key={index}>
+                        <img src={ws.img} alt="img" />
+                        <span>{ws.name}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <span>Tạo mới</span>
+                <span>
+                  <FaPlus title="Tạo bảng mới" />
+                </span>
+                <div className="navbar-dropdown">
+                  <ul>
+                    <li className="navbar-dropdown-icon-items">
+                      <div>
+                        <span>Tạo bảng mới</span>
+                        <p>
+                          Một bảng được tạo thành từ các thẻ được sắp xếp trong
+                          danh sách. Sử dụng bảng để quản lý các dự án, theo dõi
+                          thông tin, hoặc tổ chức bất cứ việc gì.
+                        </p>
+                      </div>
+                      <div>
+                        <FaTable />
+                      </div>
+                    </li>
+                    <li className="navbar-dropdown-icon-items">
+                      <div>
+                        <span>Bắt đầu với mẫu</span>
+                        <p>Bắt đầu nhanh hơn với mẫu bảng.</p>
+                      </div>
+                      <div>
+                        <FaLayerGroup />
+                      </div>
+                    </li>
+                    <li className="navbar-dropdown-icon-items">
+                      <div>
+                        <span>Tạo không gian làm việc</span>
+                        <p>
+                          Một Không gian làm việc là tập hợp các bảng và mọi
+                          người. Sử dụng Không gian làm việc để tổ chức công ty
+                          của bạn, hỗ trợ người bận rộn, gia đình hoặc bạn bè.
+                        </p>
+                      </div>
+                      <div>
+                        <FaUsers />
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className="navbar-right">
+          <div className="navbar-right-search">
+            <span>
+              <FaSearch />
+            </span>
+            <input placeholder="Tìm kiếm" />
+          </div>
+          <div className="navbar-right-icons">
+            <ul>
+              <li>
+                <span>
+                  <FaPaintRoller />
+                </span>
+                <div className="navbar-dropdown">
+                  <ul>
+                    <h5>Màu chủ đề</h5>
+                    <hr />
+                    <span onClick={() => changeTheme("default")}>Gốc</span>
+                    <span onClick={() => changeTheme("dark")}>Tối</span>
+                    <span onClick={() => changeTheme("light")}>Sáng</span>
+                    <span onClick={() => changeTheme("time")}>
+                      Theo thời gian
+                    </span>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <span>
+                  <FaBell />
+                </span>
+                <div className="navbar-dropdown navbar-dropdown-noti">
+                  <ul>
+                    <div>
+                      <h3>Thông báo</h3>
+                      <div>
+                        <label htmlFor="checkbox">
+                          Hiện những tin nhắn chưa đọc
+                          <input type="checkbox" id="checkbox" />
+                        </label>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="navbar-dropdown-noti-content">
+                      <img
+                        src="https://a.trellocdn.com/prgb/dist/images/taco-sleep.ee2660df9335718b1a80.svg"
+                        alt="no-noti"
+                      />
+                      <span>Không có thông báo nào!</span>
+                    </div>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <img
+                  className="avatar"
+                  src="https://i.ibb.co/xMdsc1K/WBS.png"
+                  alt="avatar"
+                />
+                <div className="navbar-dropdown">
+                  <ul>
+                    <h5>Tài khoản</h5>
+                    <span>Hồ sơ và hiển thị</span>
+                    <span>Hoạt động</span>
+                    <span>Thẻ</span>
+                    <span>Cài đặt</span>
+                    <hr />
+                    <span>Trợ giúp</span>
+                    <span>Phím tắt</span>
+                    <hr />
+                    <span>Đăng xuất</span>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
